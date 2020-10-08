8945eeb0fa5604d073e90d17fb32508f8d29a51d
nicodwi_k <nicodwika@gmail.com>
2020-10-08 08:15:54 +0700

    add mentorController

---
+++ app/Http/Controllers/MentorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Mentor;
+
+class MentorController extends Controller
+{
+    public function create(Request $request) {
+        $rules = [
+            'name' => 'required|string',
+            'profile' => 'required|url',
+            'email' => 'required|email',
+            'profession' => 'required|string'
+        ];
+        $data = $request->all();
+        $validation = \Validator::make($data, $rules);
+
+        if ($validation->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validation->errors()
+            ], 400);
+        }
+
+        $mentor = Mentor::create($data);
+        return response()->json([
+            'status' => 'success',
+            'data' => $mentor
+        ], 200);
+    }
+
+    public function update(Request $request, $id) {
+        $mentor = Mentor::find($id);
+        if (!$mentor) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'mentor not found'
+            ], 404);
+        }
+        
+        $rules = [
+            'name' => 'required|string',
+            'profile' => 'required|url',
+            'email' => 'required|email',
+            'profession' => 'required|string'
+        ];
+        $data = $request->all();
+        $validation = \Validator::make($data, $rules);
+
+        if ($validation->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validation->errors()
+            ], 400);
+        }
+        $mentor->update($data);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $mentor
+        ], 200);
+    }
+}
